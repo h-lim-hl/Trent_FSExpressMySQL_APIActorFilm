3c900ff8a7144ac72fae48ed1b5a297198568398
h-lim-hl <144866905+h-lim-hl@users.noreply.github.com>
2025-07-02 08:06:44 +0000

    wip actor put

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Using codespace
 To login into the database, use `mysql -u root -pmariadb -h 127.0.0.1`
+
+### Ailus
+ddl.sql = schema.sql
+dml.sql = data.sql
--- index.js
+++ index.js
@@ -1,9 +1,76 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
+
 const SERVER_PORT = 3000;
 
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "mariadb",
+    database: "sakila",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0, // 0 means infinite queue
+});
 
 const app = express();
+app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(418).json({
+        "message": "I'm a teapot!"
+    });
+});
+
+app.get("/api/", (req, res) => {
+   res.status(200).json({"message": "api online"}); 
+});
+
+
+app.get("/api/actor/", async (req, res) => {
+    // const { id, firstname, lastname } = req.query;
+    try {
+        const result = await pool.query("SELECT * FROM actor");
+        res.status(200).json({result});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({"error": err});
+    }
+});
+
+app.post("/actor", async (req, res) => {
+    const {first_name, last_name } = res.body;
+    if(!first_name || !last_name) { 
+        res.status(400).json({"error" : "Bad Request body"});
+        return;
+    };
+
+    const connection = await pool.getConnection();
+    try {
+        connection.beginTransaction();
+        let newActorId = await connection.query(`INSERT INTO actor (first_name, last_name) VALUES (? , ?);`, [first_name, last_name]);
+        connection.commit();
+        res.status(200).json({"newId" : newActorId});
+    }
+    catch (err) {
+        connection.rollback();
+        console.error(err);
+        res.status(500).json({"error": err});
+    }
+    finally {
+        connection.release();
+    }
+});
+
+app.put('/api/actor/:id', async (req, res) => {
+    const targetId = req.params.id;
+    if(!targetId){
+        res.status(400).json({"error" : "Bad ID"});
+        return;
+    } 
+
+    
+});
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server started listening on Port ${SERVER_PORT}`);
